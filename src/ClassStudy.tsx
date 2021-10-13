5f11a06588f30a87d8ac3b355af98fce8e948fec
horinoA <horinoakiko@gmail.com>
2021-10-13 23:33:34 +0900

    class作った

---
+++ src/ClassStudy.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//まずはオーソドックスにクラスを書いてstateとライフサイクルを確認する
+//https://ja.reactjs.org/docs/state-and-lifecycle.html
+class ClassStudy extends React.Component{
+    render(){
+ 
+        return(
+            <div>
+                <h1>Hello</h1>
+            </div>
+        );
+    };
+
+}
+
+export default ClassStudy
---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import ClassStudy from './ClassStudy';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+    <>
+      <Route exact path="/" component={App}/>
+      <Route exact path="/cs" component={ClassStudy}/>
+    </>
+    </Router>
+ </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
